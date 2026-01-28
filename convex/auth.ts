9fc47798cd776b34cce14de51a9759678b68c6a9
Vicente Rivera <vrivera.dev@gmail.com>
2026-01-28 15:06:45 -0300

    otro intentovich

--- convex/auth.ts
+++ convex/auth.ts
@@ -8,6 +8,7 @@ import type { DataModel } from "./_generated/dataModel";
 
 const siteUrl = process.env.SITE_URL!;
 const baseOrigin = new URL(siteUrl).origin;
+console.log("Auth base origin:", baseOrigin);
 
 // The component client has methods needed for integrating Convex with Better Auth,
 // as well as helper methods for general use.
@@ -25,6 +26,12 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
     plugins: [
       // The Convex plugin is required for Convex compatibility
       convex({ authConfig }),
+    ],
+		trustedOrigins: [
+      baseOrigin,
+      "https://*.tailf8b14c.ts.net",
+      "http://127.0.0.1:3010",
+      "http://localhost:3010",
     ],
     advanced: {
 			disableOriginCheck: true,
