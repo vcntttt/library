1a9550f3f78be9543605acc279d1c9b8580c3333
Vicente Rivera <vrivera.dev@gmail.com>
2026-01-28 14:16:26 -0300

    fuck better auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -26,20 +26,6 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
       // The Convex plugin is required for Convex compatibility
       convex({ authConfig }),
     ],
-    trustedOrigins: [
-      "http://localhost",
-      "http://localhost:*",
-      "http://127.0.0.1",
-      "http://127.0.0.1:*",
-      "https://localhost",
-      "https://localhost:*",
-      "https://127.0.0.1",
-      "https://127.0.0.1:*",
-      "http://192.168.1.8",
-      "http://192.168.1.8:*",
-      "http://192.168.1.8:3010",
-      "http://library.home",
-      "http://library.home:3010",
-    ],
+    trustedOrigins: ["*"], // fuck better auth
   });
 };
