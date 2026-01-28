b8ede508c1f745fb53110afafc17321839605119
Vicente Rivera <vrivera.dev@gmail.com>
2026-01-28 13:03:55 -0300

    fix: support origin production

--- convex/auth.ts
+++ convex/auth.ts
@@ -26,6 +26,7 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
 			"https://localhost:*",
 			"https://127.0.0.1",
 			"https://127.0.0.1:*",
+      "http://192.168.1.8"
 		],
 	});
 };
